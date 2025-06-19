3bfef3d849d47fe60dbbd5503b020b7795749cc2
ochumenno66 <ochumenno66@yandex.ru>
2025-06-19 17:06:46 +0300

    feat/promos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import MainLayout from "../../layouts/MainLayout";
-import About from './sections/About/About';
+import About from "./sections/About/About";
 import Services from "./sections/ServicesBlock/Services";
 import WhyUs from "./sections/WhyUs/WhyUs";
 // import Contacts from './sections/Contacts/Contacts';
-// import Promos from './sections/Promos/Promos';
-import Reviews from './sections/Reviews/Reviews';
+import Promos from "./sections/Promos/Promos";
+import Reviews from "./sections/Reviews/Reviews";
 import { headerConfigs } from "../../config/headerConfigs";
 import { useLocation } from "react-router-dom";
 
@@ -32,7 +32,7 @@ const HomePage = () => {
       <Services variant="dark" />
       {/* <Team variant="light" /> */}
       <Reviews variant="dark" />
-      {/* <Promos variant="promo" /> */}
+      <Promos variant="promo" />
       {/* <Contacts variant="light" /> */}
     </MainLayout>
   );
