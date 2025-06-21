0a7b602f7141259229fe1a39fa0b8cf3824d4255
Dasha <darina1970@mail.ru>
2025-06-21 18:36:27 +0700

    place animation in the centre of the block

--- src/pages/Home/sections/Animation/Animation.jsx
+++ src/pages/Home/sections/Animation/Animation.jsx
@@ -69,9 +69,10 @@ const Animation = ({ variant }) => {
     useEffect(() => {
         const handleResize = () => {
         const width = window.innerWidth;
-        if (width < 480) setFontSize(60);
-        else if (width < 768) setFontSize(90);
-        else setFontSize(120);
+        if (width < 480) setFontSize(90);
+        else if (width < 768) setFontSize(100);
+        else if (width < 1200) setFontSize(120);
+        else setFontSize(140);
         };
 
     handleResize();
@@ -81,47 +82,49 @@ const Animation = ({ variant }) => {
 
     return (
         <section id="animation" className={`${styles.section} ${styles[`section-${variant}`]}`}>
-        <div className={styles.container}>
-            <video
-            className={styles.video}
-            src="/videos/forestVideo.mp4"
-            autoPlay
-            muted
-            loop
-            playsInline
-            />
-            <svg
-            className={styles.svg}
-            viewBox="0 0 800 200"
-            preserveAspectRatio="xMidYMid meet"
-            aria-label="АЛХИМИЯ"
-            role="img"
-            >
-            <defs>
-                <mask id="text-mask">
-                <rect width="100%" height="100%" fill="white" />
-                <text
-                    x="50%"
-                    y="50%"
-                    dominantBaseline="middle"
-                    textAnchor="middle"
-                    fontFamily="Cormorant Upright, sans-serif"
-                    fontWeight="700"
-                    fontSize={fontSize}
-                    fill="black"
+            <div className={styles.container}>
+                <video
+                className={styles.video}
+                src="/videos/forestVideo.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+                />
+                <svg
+                className={styles.svg}
+                viewBox="0 0 800 200"
+                preserveAspectRatio="xMidYMid meet"
+                aria-label="АЛХИМИЯ"
+                role="img"
                 >
-                    АЛХИМИЯ
-                </text>
-                </mask>
-            </defs>
-            <rect
-                width="100%"
-                height="100%"
-                fill="#ebdcc7"
-                mask="url(#text-mask)"
-            />
-            </svg>
-        </div>
+                <defs>
+                    <mask id="text-mask">
+                    <rect width="100%" height="100%" fill="white" />
+                    <text
+                        x="50%"
+                        y="50%"
+                        // dominantBaseline="middle"
+                        textAnchor="middle"
+                        alignmentBaseline="middle"
+                        dy='0.1em'
+                        fontFamily="Cormorant Upright, sans-serif"
+                        fontWeight="700"
+                        fontSize={fontSize}
+                        fill="black"
+                    >
+                        АЛХИМИЯ
+                    </text>
+                    </mask>
+                </defs>
+                <rect
+                    width="100%"
+                    height="100%"
+                    fill="#ebdcc7"
+                    mask="url(#text-mask)"
+                />
+                </svg>
+            </div>
         </section>
     );
 };
