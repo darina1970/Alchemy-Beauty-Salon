53f452dbcd90ad615dafc05521b0f078530bf7f1
Dasha <darina1970@mail.ru>
2025-06-23 00:13:45 +0700

    add links to the other pages from home

--- src/pages/Home/sections/ServicesBlock/Services.jsx
+++ src/pages/Home/sections/ServicesBlock/Services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import hair from "../../../../assets/icons/Services/hair.svg";
 import brows from "../../../../assets/icons/Services/brows.svg";
 import hands from "../../../../assets/icons/Services/hands.svg";
@@ -14,50 +15,46 @@ const Services = ({ variant }) => {
           <div className={styles.card}>
             <img src={hair} className={styles.image} alt="Волосы" />
             <h3 className={styles.text}>Парикмахерские услуги</h3>
-            <a
-              href="#"
+            <Link to="/services/hair"
               className={`btn-primary ${styles.button}`}
               target="_blank"
               rel="noopener noreferrer"
             >
               Подробнее
-            </a>
+            </Link>
           </div>
           <div className={styles.card}>
             <img src={brows} className={styles.image} alt="Брови" />
             <h3 className={styles.text}>Бровист-ламимейкер</h3>
-            <a
-              href="#"
+            <Link to="/services/look"
               className={`btn-primary ${styles.button}`}
               target="_blank"
               rel="noopener noreferrer"
             >
               Подробнее
-            </a>
+            </Link>
           </div>
           <div className={styles.card}>
             <img src={hands} className={styles.image} alt="Руки" />
             <h3 className={styles.text}>Маникюр/Педикюр</h3>
-            <a
-              href="#"
+            <Link to="/services/nails"
               className={`btn-primary ${styles.button}`}
               target="_blank"
               rel="noopener noreferrer"
             >
               Подробнее
-            </a>
+            </Link>
           </div>
           <div className={styles.card}>
             <img src={sun} className={styles.image} alt="Солнце" />
             <h3 className={styles.text}>Солярий</h3>
-            <a
-              href="#"
+            <Link to="/services/tan"
               className={`btn-primary ${styles.button}`}
               target="_blank"
               rel="noopener noreferrer"
             >
               Подробнее
-            </a>
+            </Link>
           </div>
         </div>
       </div>
