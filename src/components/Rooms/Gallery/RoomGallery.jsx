59e4ddecd426d4555285750db23bc48fb647bca7
ochumenno66 <ochumenno66@yandex.ru>
2025-06-24 12:59:40 +0300

    корректировка работы

--- src/components/Rooms/Gallery/RoomGallery.jsx
+++ src/components/Rooms/Gallery/RoomGallery.jsx
@@ -26,26 +26,3 @@ const RoomGallery = ({ images }) => {
 };
 
 export default RoomGallery;
-
-
-/*import styles from "../Gallery/RoomGallery.module.css";
-
-const RoomGallery = ({ images }) => {
-  return (
-    <div className={`section ${styles.section}`}>
-      <h2 className={styles.title}>галерея работ</h2>
-      <div className={`container ${styles.galleryContainer}`}>
-        {images.map((img, index) => (
-          <div
-            key={index}
-            className={`${styles.galleryItem} ${styles[`item${index + 1}`]}`}
-          >
-            <img src={img.src} alt={img.alt || `${index + 1}`} loading="lazy" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default RoomGallery;*/
