f450b89d4b16e14d35d5fec369c36e80a527ca7e
ochumenno66 <ochumenno66@yandex.ru>
2025-06-24 12:51:08 +0300

    feat/rooms_gallery

--- src/components/Rooms/Gallery/RoomGallery.jsx
+++ src/components/Rooms/Gallery/RoomGallery.jsx
@@ -5,12 +5,42 @@ const RoomGallery = ({ images }) => {
     <div className={`section ${styles.section}`}>
       <h2 className={styles.title}>галерея работ</h2>
       <div className={`container ${styles.galleryContainer}`}>
+        {images.map((img, index) => {
+          const dynamicClass = styles[`item${index + 1}`] || "";
+          return (
+            <div
+              key={index}
+              className={`${styles.galleryItem} ${dynamicClass}`}
+            >
+              <img
+                src={img.src}
+                alt={img.alt ? img.alt : `Изображение ${index + 1}`}
+                loading="lazy"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RoomGallery;
+
+
+/*import styles from "../Gallery/RoomGallery.module.css";
+
+const RoomGallery = ({ images }) => {
+  return (
+    <div className={`section ${styles.section}`}>
+      <h2 className={styles.title}>галерея работ</h2>
+      <div className={`container ${styles.galleryContainer}`}>
         {images.map((img, index) => (
           <div
             key={index}
             className={`${styles.galleryItem} ${styles[`item${index + 1}`]}`}
           >
-            <img src={img.src} alt={img.alt || `Фото ${index + 1}`} />
+            <img src={img.src} alt={img.alt || `${index + 1}`} loading="lazy" />
           </div>
         ))}
       </div>
@@ -18,4 +48,4 @@ const RoomGallery = ({ images }) => {
   );
 };
 
-export default RoomGallery;
+export default RoomGallery;*/
