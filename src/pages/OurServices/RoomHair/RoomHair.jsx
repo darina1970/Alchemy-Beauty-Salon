6acc47e9abccc75993dcd2892037687d1bb2ca00
ochumenno66 <ochumenno66@yandex.ru>
2025-06-24 17:44:59 +0300

    images component

--- src/pages/OurServices/RoomHair/RoomHair.jsx
+++ src/pages/OurServices/RoomHair/RoomHair.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 import RoomGallery from "../../../components/Rooms/Gallery/RoomGallery";
-import hair1 from "../../../assets/images/Rooms/RoomHair/hair1.webp";
-import hair2 from "../../../assets/images/Rooms/RoomHair/hair2.webp";
-import hair3 from "../../../assets/images/Rooms/RoomHair/hair3.webp";
-import hair4 from "../../../assets/images/Rooms/RoomHair/hair4.webp";
-import hair5 from "../../../assets/images/Rooms/RoomHair/hair5.webp";
-import hair6 from "../../../assets/images/Rooms/RoomHair/hair6.webp";
+import hairImages from "./hairRoomImages";
 
 const HairRoom = () => {
-  const hairImages = [
-    { src: hair1, alt: "Пример работы с волосами 1" },
-    { src: hair2, alt: "Пример работы с волосами 2" },
-    { src: hair3, alt: "Пример работы с волосами 3" },
-    { src: hair4, alt: "Пример работы с волосами 4" },
-    { src: hair5, alt: "Пример работы с волосами 5" },
-    { src: hair6, alt: "Пример работы с волосами 6" },
-  ];
   return (
     <div>
       <h1>Комната волос</h1>
--- src/pages/OurServices/RoomNails/RoomNails.jsx
+++ src/pages/OurServices/RoomNails/RoomNails.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 import RoomGallery from "../../../components/Rooms/Gallery/RoomGallery";
-import nail1 from "../../../assets/images/Rooms/RoomNail/nail1.webp";
-import nail2 from "../../../assets/images/Rooms/RoomNail/nail2.webp";
-import nail3 from "../../../assets/images/Rooms/RoomNail/nail3.webp";
-import nail4 from "../../../assets/images/Rooms/RoomNail/nail4.webp";
-import nail5 from "../../../assets/images/Rooms/RoomNail/nail5.webp";
-import nail6 from "../../../assets/images/Rooms/RoomNail/nail6.webp";
+import nailImages from "./nailRoomImages";
 
 const NailsRoom = () => {
-  const nailImages = [
-    { src: nail1, alt: "Пример работы с ногтями 1" },
-    { src: nail2, alt: "Пример работы с ногтями 2" },
-    { src: nail3, alt: "Пример работы с ногтями 3" },
-    { src: nail4, alt: "Пример работы с ногтями 4" },
-    { src: nail5, alt: "Пример работы с ногтями 5" },
-    { src: nail6, alt: "Пример работы с ногтями 6" },
-  ];
   return (
     <div>
       <h1>Комната ноготков</h1>
