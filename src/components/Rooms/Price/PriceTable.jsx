a2850926b5e1dd15dbe0dfbd240cd8bb62e9ba4a
Dasha <darina1970@mail.ru>
2025-06-25 15:30:33 +0700

    styled pricetable

--- src/components/Rooms/Price/PriceTable.jsx
+++ src/components/Rooms/Price/PriceTable.jsx
@@ -2,22 +2,43 @@ import React from "react";
 import styles from './PriceTable.module.css';
 
 const PriceTable = ({ data, columns = ['Услуга', 'Цена'] }) => {
+    const showSubHeaders = columns.length > 2;
+
     return (
         <div className={styles.priceTable}>
-            <div className={styles.priceHeader}>
-                {columns.map((col, idx) => (
-                    <span key={idx} className={`price-col col-${idx}`}>{col}</span>
-                ))}
-            </div>
-            {data.map((item, index) => (
-                <div key={index} className={styles.priceRow}>
-                    {columns.map((_, idx) => (
-                        <span key={idx} className={`price-col col-${idx}`}>
-                            {item[`col${idx}`]}
+            {showSubHeaders && (
+                <div className={`${styles.priceRow} ${styles.evenRow}`}>
+                    <span className={`${styles.col} ${styles.col0}`}></span>
+                    {columns.slice(1).map((col, idx) => (
+                        <span key={idx} className={`${styles.col} ${styles.colRight}`}>
+                            {col}
                         </span>
                     ))}
                 </div>
-            ))}
+            )}
+
+            {data.map((item, rowIndex) => {
+                const isOdd = (rowIndex + (showSubHeaders ? 1 : 0)) % 2 !== 0;
+                return (
+                    <div
+                        key={rowIndex}
+                        className={`${styles.priceRow} ${
+                            isOdd ? styles.oddRow : styles.evenRow
+                        }`}
+                    >
+                        {columns.map((_, colIndex) => (
+                            <span
+                                key={colIndex}
+                                className={`${styles.col} ${
+                                    colIndex === 0 ? styles.col0 : styles.colRight
+                                }`}
+                            >
+                                {item[`col${colIndex}`]}
+                            </span>
+                        ))}
+                    </div>
+                );
+            })}
         </div>
     );
 };
