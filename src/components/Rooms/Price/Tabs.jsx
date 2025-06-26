0c76bf321d70f7f3c518902a51b08bbf971dfe20
Dasha <darina1970@mail.ru>
2025-06-26 13:39:31 +0700

    add customized select arrow and brad to the wrap tabs

--- src/components/Rooms/Price/Tabs.jsx
+++ src/components/Rooms/Price/Tabs.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import styles from './Tabs.module.css';
-
-// const Tabs = ({ tabs, activeTab, onTabClick }) => (
-//     <div className={styles.tabs}>
-//         {tabs.map((tab, index) => {
-//         const isActive = tab.key === activeTab;
-//         const isFirst = index === 0;
-//         const isLast = index === tabs.length - 1;
-
-//         const buttonClass = [
-//             styles.tab,
-//             isActive && styles.active,
-//             isFirst && styles.first,
-//             isLast && styles.last
-//         ].filter(Boolean).join(' ');
-
-//         return (
-//             <button
-//             key={tab.key}
-//             className={buttonClass}
-//             onClick={() => onTabClick(tab.key)}
-//             >
-//             {tab.label}
-//             </button>
-//         );
-//         })}
-//     </div>
-// );
-
-// export default Tabs;
-
 import React, { useEffect, useState } from "react";
 import styles from './Tabs.module.css';
 
