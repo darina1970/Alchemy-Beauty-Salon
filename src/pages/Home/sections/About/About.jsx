c06de04940b5322a22bcf13ba87bcb4c4bdc70a0
Ninima1218 <ninima1218@gmail.com>
2025-06-27 23:56:06 +0400

    fix block styles

--- src/pages/Home/sections/About/About.jsx
+++ src/pages/Home/sections/About/About.jsx
@@ -10,15 +10,11 @@ const About = ({ variant }) => {
           <h2>О НАС</h2>
         </div>
 
-        <div className={styles.imageWrapper}>
-          <img src={owner} alt="Основатель салона" className={styles.aboutImage} />
-        </div>
-
         <div className={styles.textBlock}>
           <h2>О НАС</h2>
 
           <p className={styles.text}>
-            АЛХИМИЯ - это не просто салон красоты, это настоящая бьюти-лаборатория, спрятанная под землей, где происходят волшебные превращения.
+            АЛХИМИЯ — это не просто салон красоты, это настоящая бьюти-лаборатория, спрятанная под землей, где происходят волшебные превращения.
           </p>
 
           <p className={styles.text}>
@@ -32,6 +28,10 @@ const About = ({ variant }) => {
           <p className={styles.text}>
             Добро пожаловать в АЛХИМИЮ — место, где красота рождается из волшебства.
           </p>
+        </div>
+
+        <div className={styles.imageWrapper}>
+          <img src={owner} alt="Основатель салона" className={styles.aboutImage} />
         </div>
       </div>
     </section>
